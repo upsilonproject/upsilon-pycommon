5aa56b8021aaee01fe957d39b6451a3d5d44c387
jamesread <contact@jwread.com>
2016-09-29 12:14:28 +0100

    .

--- make.sh
+++ make.sh
@@ -3,4 +3,5 @@ rm -rf pkg
 mkdir -p pkg
 
 buildid -n
-zip -r pkg/upsilon-pycommon-$(buildid -k tag).zip .buildid src/*.py pkg
+buildid -qf rpmmacro -W .upsilon-pycommon.rpmmacro
+zip -r pkg/upsilon-pycommon-$(buildid -k tag).zip .buildid src/*.py pkg var .upsilon-pycommon.rpmmacro
