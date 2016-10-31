a7a24d5b341d50ecdf0e22b6880f9cdcb1d84b8f
jamesread <contact@jwread.com>
2016-10-31 11:34:04 +0000

    Added support for pings and stuff.

--- src/amqp.py
+++ src/amqp.py
@@ -1,9 +1,30 @@
 import pika
 import uuid
 
+class UpsilonMessage():
+    routingKey = "*"
+    headers = {}
+    replyTo = ""
+    body = ""
+
+    def __init__(self, msgType):
+        if msgType != None:
+            self.headers["upsilon-msg-type"] = msgType
+
+    def getProperties(self): 
+        return pika.BasicProperties(
+            headers = self.headers,
+            reply_to = self.replyTo
+            );
+
+
 class Connection():
     messageHandlers = []
     messageTypeHandlers = {}
+
+    nodeIdentifier = "???";
+    nodeVersion = "?.?.?"
+    nodeType = "???";
 
     def __init__(self, host = None, queue = None, exchange = "ex_upsilon", callback = None):
         if host == None:
@@ -23,7 +44,33 @@ class Connection():
         if callback != None:
             self.addMessageHandler(callback)
 
+        if replyToPings:
+
         self.channel.basic_consume(self.callbackHelper, queue = self.queue)
+
+    def setPingReply(self, identifier = "???", version = "?.?.?", traits = "???"):
+        self.nodeIdentifier = identifier;
+        self.nodeVersion = version;
+        self.nodeType = traits;
+
+        self.addMessageTypeHandler("REQ_NODE_SUMMARY", self.onPing)
+
+    def onPing(self, channel, delivery, properties, body):
+        msg = UpsilonMessage("RES_NODE_SUMMARY")
+        msg.routingKey = "upsilon.res"
+        msg.headers["node-identifier"] = self.nodeIdentifier;
+        msg.headers["node-version"] = self.nodeVersion;
+        msg.headers["node-type"] = self.nodeType;
+
+        self.publishMessage(msg)
+        
+    def publish(self, key, headers, body, replyTo = None):
+        properties = pika.BasicProperties(headers = headers, reply_to = replyTo)
+
+        self.channel.basic_publish(exchange = self.exchange, routing_key = key, properties = properties, body = body) 
+
+    def publishMessage(self, msg):
+        self.channel.basic_publish(exchange = self.exchange, routing_key = msg.routingKey, properties = msg.getProperties(), body = msg.body)
 
     def close(self):
         print "closing"
@@ -48,13 +95,13 @@ class Connection():
     def callbackHelper(self, channel, delivery, properties, body):
         if "upsilon-msg-type" in properties.headers:
             msgType = properties.headers["upsilon-msg-type"]
-
+            
             if msgType in self.messageTypeHandlers:
                 callback = self.messageTypeHandlers[msgType]
                 callback(channel, delivery, properties, body)
 
         for callback in self.messageHandlers:
-            callback(channel, delivery, headers, body)
+            callback(channel, delivery, properties, body)
         
     def startConsuming(self):
         self.channel.start_consuming()
@@ -66,6 +113,8 @@ def newChannel(host, queue, exchange = "ex_upsilon"):
 
         if not amqpConnection.is_open:
             raise Exception("Could not open a connection")
+
+        print "Connection open"
 
 	channel = amqpConnection.channel();
 	channel.queue_declare(queue = queue, durable = False, auto_delete = True)
