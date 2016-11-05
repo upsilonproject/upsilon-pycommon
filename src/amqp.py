2aa2bd2ceab5ab0dbea14b8040897aecd85c22f4
jamesread <contact@jwread.com>
2016-11-05 10:25:12 +0000

    Log the hostname on connection

--- src/amqp.py
+++ src/amqp.py
@@ -112,7 +112,7 @@ def newChannel(host, queue, exchange = "ex_upsilon"):
         if not amqpConnection.is_open:
             raise Exception("Could not open a connection")
 
-        print "Connection open"
+        print "Connection open to", host, ' using the', exchange, 'exchange'
 
 	channel = amqpConnection.channel();
 	channel.queue_declare(queue = queue, durable = False, auto_delete = True)
